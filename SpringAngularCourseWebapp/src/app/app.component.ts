1564785e8dcf3365f78815403a85c234c61158a7
jose de leon <joseleon1903@outlook.com>
2021-04-20 19:24:22 -0400

    add angular create and update functionality

--- SpringAngularCourseWebapp/src/app/app.component.ts
+++ SpringAngularCourseWebapp/src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Employee} from "./Employee";
 import {EmployeeService} from "./employee.service";
 import {HttpErrorResponse} from "@angular/common/http";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,7 @@ export class AppComponent implements OnInit{
   title = 'SpringAngularCourseWebapp';
 
   public employees : Employee[];
+  public editEmployee : Employee;
 
   constructor(private employeeService : EmployeeService){
     this.employees = [];
@@ -35,5 +37,66 @@ export class AppComponent implements OnInit{
       }
     );
   }
+
+
+  public onOpenModal(employee : Employee, mode: string): void {
+
+    console.log("entering in onOpenModal");
+    const button = document.createElement('button');
+
+    const container = document.getElementById('main-container');
+
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle','modal');
+
+    if(mode == 'add'){
+      button.setAttribute('data-target','#addEmployeeModal');
+    }
+
+    if(mode == 'edit'){
+      this.editEmployee = employee;
+      button.setAttribute('data-target','#updateEmployeeModal');
+    }
+
+    if(mode == 'delete'){
+      button.setAttribute('data-target','#deleteEmployeeModal');
+    }
+
+    container.appendChild(button);
+
+    button.click();
+
+  }
+
+
+  public onAddEmployee(addForm : NgForm){
+
+    document.getElementById('add-employee-form').click();
+    this.employeeService.addEmployees(addForm.value).subscribe(
+      (response:Employee)=> {
+        console.log("response:"+response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse)=>{
+
+      }
+    );
+  }
+
+  public onUpdateEmployee(employee : Employee) : void{
+
+    document.getElementById('add-employee-form').click();
+    this.employeeService.updateEmployees(employee).subscribe(
+      (response:Employee)=> {
+        console.log("response:"+response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse)=>{
+
+      }
+    );
+  }
+
 
 }
